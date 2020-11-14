a10c72a2698db8af4dc40f60e22192da97667582
chenmaker <34946182+chenmaker@users.noreply.github.com>
2020-11-14 21:30:52 +0800

    Add files via upload

---
+++ 戰艦程式/NRF24版-3/battleship-receive/TR.ino
@@ -0,0 +1,15 @@
+void tr(){//溫度
+  /*Serial.print("The sensor temperature is: ");
+  Serial.print(temperature);
+  Serial.println("C");
+  Serial.print("The sensor temperature1 is: ");
+  Serial.print(temperature1);
+  Serial.println("C");
+  if(temperature>40||temperature1>40){
+     digitalWrite(L7,HIGH);
+                }
+  else if(temperature<39||temperature<39)
+      {digitalWrite(L7,LOW);
+              }
+  }*/
+}
